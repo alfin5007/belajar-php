42ef93d32376cfeeba080e52a75b903649de3806
alfin5007 <alfanalfin507@gmail.com>
2026-04-22 15:55:13 +0800

    penambahan

---
+++ 29-require-include/index.php
@@ -0,0 +1,8 @@
+<?php
+include "rumus.php";
+
+$luaspp = luaspersegipanjang(5,3);
+echo "luas persegi panjang: $luaspp ". PHP_EOL;
+
+$luas = luaspersegi(5);
+echo "luas persegi: " .$luas; 
---
+++ 29-require-include/rumus.php
@@ -0,0 +1,13 @@
+<?php
+
+// rumus luas persegi panjang
+function luaspersegipanjang($panjang, $lebar){
+    $luas = $panjang * $lebar;
+    return $luas;
+}
+
+// rumus luas persegi
+function luaspersegi($sisi){
+    $luas = $sisi * $sisi;
+    return $luas;
+}
